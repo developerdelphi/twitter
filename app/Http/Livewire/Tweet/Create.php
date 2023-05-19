f21df93cd3c5d7ace24338464de0f0ff0034374f
Oleomar Buchner <developerdelphi@gmail.com>
2023-05-19 09:29:19 -0300

    Sure that only authenticated users

---
+++ app/Http/Livewire/Tweet/Create.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Livewire\Tweet;
+
+use App\Models\Tweet;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Livewire\Component;
+use View;
+
+class Create extends Component
+{
+    use AuthorizesRequests;
+    public ?string $body = null;
+
+
+    public function render()
+    {
+        return view('livewire.tweet.create');
+    }
+
+    public function tweet()
+    {
+        $this->authorize('create',Tweet::class);
+
+        Tweet::query()->create([
+            'body'=> $this->body,
+            'created_by' => auth()->id(),
+        ]);
+
+        $this->emit('tweet::created');
+    }
+}
--- tests/Feature/TweetTest.php
+++ tests/Feature/TweetTest.php
@@ -1,12 +1,44 @@
 <?php
+use App\Http\Livewire\Tweet\Create;
+use App\Models\Tweet;
+use App\Models\User;
+use function Pest\Laravel\actingAs;
+use function Pest\Laravel\assertDatabaseCount;
+use function Pest\Livewire\livewire;
 
-todo('Should be albe to create a tweet', function(){
-    livewire(Tweet::class)
+it('Should be able to create a tweet', function(){
+    $user = User::factory()->create();
+
+    actingAs($user);
+
+    livewire(Create::class)
         ->set('body','This is my first tweet')
         ->call('tweet')
         ->assertEmitted('tweet::created');
-    \Pest\Laravel\assertDatabaseCount('tweets', 1);
+
+    assertDatabaseCount('tweets', 1);
+
+    expect(Tweet::first())
+    ->body->toBe('This is my first tweet')
+    ->created_by->toBe($user->id);
 });
+
+it('Should make sure that only authenticated users can tweet', function(){
+    livewire(Create::class)
+        ->set('body', 'This is my first tweet')
+        ->call('tweet')
+        ->assertForBidden();
+        // ->assertRedirect(route('login'));
+
+    actingAs(User::factory()->create());
+
+    livewire(Create::class)
+        ->set('body','This is my first tweet')
+        ->call('tweet')
+        ->assertEmitted('tweet::created');
+
+});
+
 todo('body is requirede');
 todo('the tweet should have a max length of 140 chars');
 todo('should show the tweet on the timeline');
