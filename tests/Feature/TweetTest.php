1fe50e5bd2fe44199a2f9aa01940966a616a7252
Oleomar Buchner <developerdelphi@gmail.com>
2023-05-19 08:57:27 -0300

    add livewire

--- tests/Feature/TweetTest.php
+++ tests/Feature/TweetTest.php
@@ -1,6 +1,12 @@
 <?php
 
-todo('Should be albe to create a tweet');
+todo('Should be albe to create a tweet', function(){
+    livewire(Tweet::class)
+        ->set('body','This is my first tweet')
+        ->call('tweet')
+        ->assertEmitted('tweet::created');
+    \Pest\Laravel\assertDatabaseCount('tweets', 1);
+});
 todo('body is requirede');
 todo('the tweet should have a max length of 140 chars');
 todo('should show the tweet on the timeline');
